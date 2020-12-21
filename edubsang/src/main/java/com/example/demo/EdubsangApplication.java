ae2fbb1f7d4a9580d606b1b26c5d1ee011a2b701
SangjunBae <tkacn23@gmail.com>
2020-12-22 02:15:37 +0900

    first commit

---
+++ edubsang/src/main/java/com/example/demo/EdubsangApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdubsangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdubsangApplication.class, args);
+	}
+
+}
